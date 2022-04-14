a07fdca734694c9ba5ae06a4b4fa95c51289ee7f
Benjamin Staubli <benjamin.staubli@airlaunch.ch>
2022-04-14 09:52:14 +0200

    shift to 12 hours

--- dag.py
+++ dag.py
@@ -65,7 +65,7 @@ with DAG(
     'opc',
     default_args=default_args,
     description='Extract Production Data',
-    schedule_interval=timedelta(minutes=15),
+    schedule_interval=timedelta(hours=12),
     start_date=datetime(2022, 1, 16, 1, 0, 0),
     catchup=True,
 ) as dag:
